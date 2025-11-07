9e51f38b3f27e5134349fa20f02010f2b16664e2
ob1 <ongjjbrandon@gmail.com>
2025-11-07 14:30:59 +0000

    run puz on first round of models

--- eval/tts_eval/step_aggregation_search/run_prm_eval_updated.sh
+++ eval/tts_eval/step_aggregation_search/run_prm_eval_updated.sh
@@ -8,10 +8,10 @@ export PYTHONPATH="${PYTHONPATH}:/home/ubuntu/poria-cvpr-2026/ob1/vlprm/"
 
 OUTPUT_DATA_DIR="/home/ubuntu/poria-cvpr-2026/ob1/vlprm/eval/tts_eval/step_aggregation_search/outputs"
 # BASE_DATA_DIR="/home/ubuntu/poria-cvpr-2026/ob1/vlprm/eval/tts_eval/reward_guided_search/VisualPRM_relabelling"
-INPUT_JSON_DATA_PATH="/home/ubuntu/poria-cvpr-2026/Tej/mmr-eval/traces_data/g12b_policy_step/q3b_prm/Q3B_mc0_sr_mc0_full_bs2_gs4_lr1e-5_VF_0827_1452_AlgoPuzzleVQA_900_subset_result-merged-0-900-20250903_145022.json"
+INPUT_JSON_DATA_PATH="/home/ubuntu/poria-cvpr-2026/Tej/mmr-eval/traces_data/g12b_policy_step/q3b_prm/Q3B_mc0_sr_mc0_full_bs2_gs4_lr1e-5_VF_0827_1452_puzzleVQA_1K_subset_result-merged-0-1000-20250902_232205.json"
 
 CHECKPOINT_BASE_PATH="/home/ubuntu/poria-cvpr-2026/ob1/CVPR_PRM/src/training/trained_models"
-MODEL_PATH="${CHECKPOINT_BASE_PATH}/ob11_Qwen-VL-PRM-3B_grpo_listwise_ce_20251106_210950"
+MODEL_PATH="${CHECKPOINT_BASE_PATH}/ob11_Qwen-VL-PRM-7B_grpo_listwise_ce_20251106_215426"
 # MODEL_PATH="ob11/Qwen-VL-PRM-3B"
 # MODEL_PATH="OpenGVLab/VisualPRM-8B-v1_1"
 
@@ -44,43 +44,42 @@ fi
 # Determine model prefix and add suffix based on whether it's finetuned or base
 # Include datetime from MODEL_PATH in model_prefix if available
 if [[ $MODEL_PATH =~ [V]isualPRM-8B ]]; then
-    model_prefix="V8B"
+    reward_model_prefix="V8B"
 elif [[ $MODEL_PATH =~ [V]isualPRM-8B-v1_1 ]]; then
-    model_prefix="V8B-v1_1"
+    reward_model_prefix="V8B-v1_1"
 elif [[ $MODEL_PATH =~ [Q]wen-VL-PRM-3B ]]; then
     if [[ $MODEL_PATH == ${CHECKPOINT_BASE_PATH}* ]]; then
-        model_prefix="VL-3B-ft"
+        reward_model_prefix="VL-3B-ft"
     else
-        model_prefix="VL-3B-base"
+        reward_model_prefix="VL-3B-base"
     fi
 elif [[ $MODEL_PATH =~ [Q]wen-VL-PRM-7B ]]; then
     if [[ $MODEL_PATH == ${CHECKPOINT_BASE_PATH}* ]]; then
-        model_prefix="VL-7B-ft"
+        reward_model_prefix="VL-7B-ft"
     else
-        model_prefix="VL-7B-base"
+        reward_model_prefix="VL-7B-base"
     fi
 else
-    model_prefix="UNKNOWN"  # fallback
+    reward_model_prefix="UNKNOWN"  # fallback
 fi
+
+POLICY_MODEL="g12b_policy_step"
 
 # Append datetime from MODEL_PATH to model_prefix if available
 if [[ -n "$model_datetime" ]]; then
-    model_prefix="${model_prefix}_${model_datetime}"
+    reward_model_prefix="${reward_model_prefix}_${model_datetime}"
 fi
 
-# RUN_SETTING="step_agg"
-RUN_SETTING="non_greedy"
+RUN_SETTING="step_agg"
+# RUN_SETTING="non_greedy"
 
-base_job_name_prefix="PRM_${model_prefix}"
+base_job_name_prefix="PRM_${reward_model_prefix}"
 
-mkdir -p logs/inference_logs/single_runs/prm_${model_prefix}
+mkdir -p logs/inference_logs/${POLICY_MODEL}/prm_${reward_model_prefix}
 
-CUDA_VISIBLE_DEVICES=3 python prm_tts_eval.py \
+CUDA_VISIBLE_DEVICES=4 python prm_tts_eval.py \
     --model-path $MODEL_PATH \
     --data-path ${INPUT_JSON_DATA_PATH} \
-    --output-path ${OUTPUT_DATA_DIR}/single_runs/prm_${model_prefix}/${dataset_shorthand}_${RUN_SETTING}-${current_datetime}.json \
+    --output-path ${OUTPUT_DATA_DIR}/${POLICY_MODEL}/prm_${reward_model_prefix}/${dataset_shorthand}_${RUN_SETTING}-${current_datetime}.json \
     --tts-type ${RUN_SETTING} \
-    2>&1 | tee logs/inference_logs/single_runs/prm_${model_prefix}/${dataset_shorthand}_${RUN_SETTING}-${current_datetime}
-
-# Change to step_agg for step_aggregation
-#     --tts-type step_agg \
+    2>&1 | tee logs/inference_logs/${POLICY_MODEL}/prm_${reward_model_prefix}/${dataset_shorthand}_${RUN_SETTING}-${current_datetime}
